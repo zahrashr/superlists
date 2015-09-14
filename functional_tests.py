f2d11d32c081955556b1a74f856f7c3546ae93cb
vagrant <zahra.sharfina@ui.ac.id>
2015-09-14 07:00:09 +0000

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
-
+		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,9 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
+
 		#self.assertTrue(response.content.startswith(b'<html>'))
 		#self.assertIn(b'<title>To-Do lists</title>', response.content)
 		#self.assertTrue(response.content.strip().endswith(b'</html>'))
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
