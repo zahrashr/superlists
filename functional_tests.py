2c17e3213649822dde988e37307afd58002ef68d
vagrant <zahra.sharfina@ui.ac.id>
2015-09-14 07:42:59 +0000

    Yeay

--- functional_tests.py
+++ functional_tests.py
@@ -28,12 +28,15 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-		"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
