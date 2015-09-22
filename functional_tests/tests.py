608cd2bc0924b9d9f0f792b596d2ecbc74c3f0e0
vagrant <zahra.sharfina@ui.ac.id>
2015-09-22 17:06:26 +0000

    tes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,6 +34,8 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 				
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -42,11 +44,31 @@ class NewVisitorTest(LiveServerTestCase):
 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('make a fly', page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		#table = self.browser.find_element_by_id('id_list_table')
+		#rows = table.find_elements_by_tag_name('tr')
+		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
